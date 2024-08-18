4ce6af77db3fd8773b89f57eeb1890a1ec5bd9ff
akyrey <stein.dario@gmail.com>
2024-08-18 18:42:48 +0200

    chore: fix https for all urls

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -19,6 +19,12 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        // Use HTTPS for all URLs
+        if (
+            (isset($_SERVER['HTTPS']) && ($_SERVER['HTTPS'] == 'on' || $_SERVER['HTTPS'] == 1))
+            || (isset($_SERVER['HTTP_X_FORWARDED_PROTO']) && $_SERVER['HTTP_X_FORWARDED_PROTO'] == 'https')
+        ) {
+            $this->app['request']->server->set('HTTPS', true);
+        }
     }
 }
